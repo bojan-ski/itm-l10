844cd625aece5e4c8ba190c7e57e47c8bd9e49e7
Bojan SKI <codeucenje@gmail.com>
2024-03-11 09:52:40 +0100

    lecture done, start work on homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 import Payment from "./components/Payment"
 
-export const CurrencyContext = createContext("USD")
+export const CurrencyContext = createContext()
 // console.log(CurrencyContext);
 
+export const AmountContext = createContext()
+
 function App() {
+  const [currency, setCurrency] = useState('USD')
+  const [amount, setAmount] = useState(0)
+
+  const updateCurrency = () => {
+    setCurrency("EURO")
+  }
+  // console.log(currency);
+
+  const updateAmount = (value) => {
+    setAmount(value)
+  }
+  // console.log(amount);
+
   return (
-    <> 
-      <CurrencyContext.Provider>
-        <div className="container">
-          <Payment />
-        </div>
+    <>
+      <CurrencyContext.Provider value={{ currency, updateCurrency }}>
+        <AmountContext.Provider value={{ amount, updateAmount }}>
+          <div className="container">
+            <Payment />
+
+            <input type="text" onInput={(e) => updateAmount(e.target.value)} />
+
+            <button onClick={updateCurrency}>
+              Click
+            </button>
+          </div>
+        </AmountContext.Provider>
       </CurrencyContext.Provider>
     </>
   )
--- src/components/PayPal.jsx
+++ src/components/PayPal.jsx
@@ -1,4 +1,12 @@
+import { useContext } from 'react'
+import { AmountContext, CurrencyContext } from '../App'
+
 const PayPal = () => {
+    const currency = useContext(CurrencyContext)
+    const amount = useContext(AmountContext)
+    // console.log(currency);
+    // console.log(amount);
+
     return (
         <div>PayPal</div>
     )
